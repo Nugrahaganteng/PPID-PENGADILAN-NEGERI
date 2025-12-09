6622c5852993fe8c9389a4d1790eb50ca7a0527b
Nugrahaganteng <nugrahafirizki@gamil.com>
2025-12-09 14:31:54 +0700

    up

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Load the Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
